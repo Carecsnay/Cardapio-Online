8149f432372fb8e2d685f2998104d8c1b7037b5d
Bruno Benicio (Castelo do Piauí-PI) <carecsnay1@gmail.com>
2024-03-16 15:46:46 -0300

    Feat: Add JS selecting elements

---
+++ script.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector('#modal');
+const menu = document.querySelector('#menu');
+const cartBTN = document.querySelector('#cart-btn');
+const cartItemsContainer = document.querySelector('#card-items');
+const cartTotal = document.querySelector('#cart-total');
+const checkoutBTN = document.querySelector('#checkout');
+const closeModalBTN = document.querySelector('#close-modal-btn');
+const cardCounter = document.querySelector('#cart-count');
+const addressInput = document.querySelector('#address');
+const warnInput = document.querySelector('#address-warn');
+
