b8bfd96792fd401920ff0d5315ee8fc9daa186db
Bruno Benicio (Castelo do Piauí-PI) <carecsnay1@gmail.com>
2024-03-16 20:23:29 -0300

    Fix: ID: "cart-items" and Feat: Informations modal

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ cartBTN.addEventListener('click', () => {
 });
 
 modal.addEventListener('click', (e) => {
-    if(e.target === modal) {
+    if (e.target === modal) {
         modal.style.display = "none";
     }
 });
@@ -27,27 +27,52 @@ closeModalBTN.addEventListener('click', () => {
 
 menu.addEventListener('click', (e) => {
     let parentButton = e.target.closest(".add-to-cart-btn");
-    
+
     if (parentButton) {
         const name = parentButton.getAttribute('data-name');
         const price = parseFloat(parentButton.getAttribute('data-price')).toFixed(2);
 
-        addToCart(name,price);
+        addToCart(name, price);
     }
 });
 
-function addToCart (name, price){
+function addToCart(name, price) {
     const existingItem = cart.find(item => item.name === name)
 
-    if(existingItem) {
-        existingItem.quantity ++;
-        return;
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        });
     }
 
-    cart.push({
-        name,
-        price,
-        quantity: 1,
-    });
+    updateModal()
 }
 
+function updateModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement('div');
+        cartItemElement.innerHTML = `
+        <div>
+            <div>
+                <p>${item.name}</p>
+                <p>${item.quantity}</p>
+                <p>$R$ ${item.price}</p>
+            </div>
+
+            <div>
+                <button>
+                    Remover
+                </button>
+            </div>
+        </div>
+        `
+        cartItemsContainer.appendChild(cartItemElement);
+    })
+}
