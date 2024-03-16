1d029794123db7d79e337971eba4fdf8b6d8661f
Bruno Benicio (Castelo do Piauí-PI) <carecsnay1@gmail.com>
2024-03-16 16:08:05 -0300

    Feat: Function capture item selected.

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ modal.addEventListener('click', (e) => {
 closeModalBTN.addEventListener('click', () => {
     modal.style.display = "none";
 });
+
+menu.addEventListener('click', (e) => {
+    let parentButton = e.target.closest(".add-to-cart-btn");
+    
+    if (parentButton) {
+        const name = parentButton.getAttribute('data-name');
+        const price = parseFloat(parentButton.getAttribute('data-price'));
+        
+    }
+});
